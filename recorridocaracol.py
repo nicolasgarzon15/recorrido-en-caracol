150b7be9731d97d9e99aa9f591cd7793b79c931f
Nicolas Garzon Vasquez <43526116+nicolasgarzon15@users.noreply.github.com>
2019-02-05 21:14:47 -0500

    Update recorridocaracol.py

--- recorridocaracol.py
+++ recorridocaracol.py
@@ -1,1 +1,44 @@
-.
+import numpy as np
+from io import StringIO
+
+pfile=open('arreglo','r')
+data=pfile.read()
+pfile.close()
+data=np.genfromtxt(StringIO(data), delimiter  = ',' )
+
+x= list(data.shape)
+filas=x[0]
+columnas=x[1]
+sup=0
+izq=0
+der=columnas
+inf=filas
+numeros=[]
+
+print(data)
+print(filas)
+print(columnas)
+
+while sup < inf or izq < der :
+    i=inf-1
+    j=der-1
+
+    while j>izq:
+        numeros.append(data[i][j])
+        j=j-1
+    while i>sup:
+        numeros.append(data[i][j])
+        i=i-1
+    while j<der-1:
+        numeros.append(data[i][j])
+        j=j+1
+    while i<inf-1:
+        numeros.append(data[i][j])
+        i=i+1
+
+    izq=izq+1
+    sup=sup+1
+    der=der-1
+    inf=inf-1
+
+print(numeros)
